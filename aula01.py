4d4db290c64c66f147d1dbfd8ce22dea5b11549a
Luiz Correia <104090205+Luizinhjk@users.noreply.github.com>
2024-08-16 11:59:26 -0300

    Add files via upload

---
+++ aula01.py
@@ -0,0 +1,10 @@
+nome = "Luiz"
+idade = 16
+
+print("olá", nome, "a sua idade é", idade)
+
+nome = "Cleo pires"
+idade = 34
+altura = 1.73
+
+print("olá", nome, "a sua idade é", idade, "a sua altura é", altura)
